b4fef2d25f4d34d7b526d6db6928b2c2b06caeae
loganmiller001 <loganmiller001@live.com>
2018-06-13 15:21:50 -0500

    monster generated

--- finaldice.js
+++ finaldice.js
@@ -4,6 +4,16 @@ let player = prompt("Enter player name");
 	return (player);
 }
 
+function playerClass(){
+	let charClass = prompt("Enter your character class");
+		return (charClass);
+}
+
+function playerRace(){
+	let charRace = prompt("Enter your character race");
+		return charRace;
+}
+
 function rollSingleDice(n){
 	return Math.floor(Math.random()*n + 1);
 }
@@ -25,6 +35,7 @@ function rollDamage(n){
 	console.log(damNumber);
 	return damNumber;
 }
+
 function rollStatModifier(n){
 	let statModifier = rollSingleDice(8);
 	console.log(statModifier);
@@ -67,8 +78,7 @@ function rollCharisma(n,statModifier){
 	return charisma;
 }
 
-
-function characterInitative(n){
+function characterInitiative(n){
 	let baseInitiative = rollSingleDice(6);
 	console.log(baseInitiative);
 	return baseInitiative;
@@ -80,9 +90,10 @@ function attackRoll(n){
 	return attackScore;
 }
 
-
 function characterGenerator(){
 	let characterName = playerName();
+	let characterClass = playerClass();
+	let characterRace = playerRace();
 	let health = rollHp();
 	let toHit = attackRoll()
 	let ac = rollAc();
@@ -92,9 +103,12 @@ function characterGenerator(){
 	let attributeInt = rollIntelligence();
 	let attributeWis = rollWisdom();
 	let attributeChr = rollCharisma();
-	let initative = characterInitative();
+	let playerInitiative = characterInitiative();
 	console.log("Your name is: " + characterName);
+	console.log("Your class is: " + characterClass);
+	console.log("Your race is: " + characterRace);
 	console.log("Your max health points: " + health);
+	console.log("Your hit roll was: " + toHit); 
 	console.log("Your armor class is: " + ac);
 	console.log("You Strength is: " + attributeStr);
 	console.log("Your Dexterity is: " + attributeDex);
@@ -102,14 +116,71 @@ function characterGenerator(){
 	console.log("Your Intelligence is: " + attributeInt);
 	console.log("Your Wisdom is: " + attributeWis);
 	console.log("Your Charisma is: " + attributeChr);
-	console.log("Your initative is: " + initative);
+	console.log("Your initiative is: " + playerInitiative);
 
 }
-characterGenerator()
+alert("You encounter a monster");
 
-// alert("You encounter a monster");
+function monsterAttack(n){
+	let attack = rollSingleDice(20);
+	console.log(attack);
+		return attack;
+}
 
-// let monster = {
-// 	name: "Kobald",
-// 	health: "10"
+function monsterDamage(n){
+	let damage = rollSingleDice(8);
+	console.log(damage);
+		return damage;
+}
+
+function monsterHp(n){
+	let hp = rollSingleDice(12);
+	console.log(hp);
+		return hp;
+}
+
+function monsterArmor(n){
+	let enemyAc = rollSingleDice(6);
+	console.log(enemyAc);
+		return enemyAc;
+}
+
+function monsterInitiative(n){
+	let enemyInit = rollSingleDice(4);
+	console.log(enemyInit);
+		return enemyInit
+}
+
+function monsterGenerator(){
+	let enemyHealth = monsterHp();
+	let enemyAttack = monsterAttack();
+	let enemyDamage = monsterDamage();
+	let enemyArmor = monsterArmor();
+	let enemyInitiative = monsterInitiative();
+	console.log("Enemy max health is: " + enemyHealth);
+	console.log("Enemy attack roll: " + enemyAttack);
+	console.log("Enemy damage roll is: " + enemyDamage);
+	console.log("Enemy armor class is: " + enemyArmor);
+	console.log("Enemy initiative is: " + enemyInitiative);
+}
+
+function runGame(){
+	let player = characterGenerator();
+	let monster = monsterGenerator();
+	
+}
+// function runGame(){
+	//let player = characterGenerator();
+	//let monster = monsterGenerator();
+	 //player.health -=monsterAttack(); 
 // }
+// let character = object{
+	// character.health = rollHp(),
+// }
+// 
+
+runGame()
+
+
+
+
